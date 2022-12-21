8752464571e51a51b1f44918780abd4bd88c9ff6
vagap <you@example.com>
2022-12-21 16:20:13 +0300

    homework1

---
+++ homework/Program.cs
@@ -0,0 +1,86 @@
+﻿/*  // 1 задание
+Console.Write("ведите 1 число  :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("ведите 2 число  :");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1 > num2)
+{
+    int max = num1;
+    int min = num2;
+    Console.WriteLine("максимум   :" + max);
+    Console.WriteLine("минимум   :" + min);
+}
+else 
+{
+    int max = num2;
+    int min = num1;
+    Console.WriteLine("максимум  :" + max);
+    Console.WriteLine("минимум   :" + min);
+}
+*/
+
+/*   //2 задание
+Console.Write("ведите число  1 :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("ведите число  2 :");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("ведите число  3 :");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max)
+{
+    max = num2;
+    Console.Write(max);
+}
+if(num3 > max)
+{
+    max = num3;
+    Console.Write(max);
+}
+*/
+
+/*  //3 задание
+
+Console.Write("ведите число  :");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("четное");
+}
+
+else
+{
+    Console.WriteLine("не четное");
+}
+*/
+
+/*    // 4 задание
+
+Console.Write("Пожалуйста введите число  :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+ if (num1 <= 1)
+{
+    Console.Write("Не четное число");
+
+}
+else
+  while (current <= num1 )
+    {
+        
+    if (current % 2 == 0)
+    {
+        Console.Write (current + " ");
+        current = current + 2; 
+    }
+     else
+    {
+        current = current + 1;  
+    }
+}
+    
+*/
